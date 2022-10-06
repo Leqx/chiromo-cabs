2dd09400fb03e641ce49838acf08e4b7a00c36cb
leqx <eugenealex123@gmail.com>
2022-10-07 02:47:15 +0300

    Initial commit

---
+++ src/main/java/com/chiromo/chiromocabs/ChiromoCabsApplication.java
@@ -0,0 +1,13 @@
+package com.chiromo.chiromocabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiromoCabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiromoCabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chiromo/chiromocabs/ChiromoCabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiromo.chiromocabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiromoCabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
